4ebbffdc4a9f5c6c65261583fffdda9d8e87dbab
Jasa Plohl <jasa.plohl@devleet.si>
2023-07-04 08:21:08 +0200

    Define the base components.

--- match-carousel/src/components/App.js
+++ match-carousel/src/components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import MatchCarousel from "./MatchCarousel";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+          <p>Navigation between the two tabs</p>
       </header>
+      <MatchCarousel />
     </div>
   );
 }
---
+++ match-carousel/src/components/MatchCarousel.js
@@ -0,0 +1,14 @@
+import './MatchCarousel.css';
+import Card from "./Card";
+
+const MatchCarousel = () => {
+    return(
+        <div className="carousel">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    );
+};
+
+export default MatchCarousel;
